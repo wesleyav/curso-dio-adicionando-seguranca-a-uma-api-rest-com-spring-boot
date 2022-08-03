47d3838bc2cfd7dd6f94ca5ab79edfa620a4260f
Wesley AV <13851280+wesleyav@users.noreply.github.com>
2022-08-03 17:40:17 -0300

    JWT - JSON Web Token - Parte 2

---
+++ dio-spring-security-jwt/src/main/java/com/github/wesleyav/diospringsecurityjwt/repository/UserRepository.java
@@ -0,0 +1,15 @@
+package com.github.wesleyav.diospringsecurityjwt.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
+import com.github.wesleyav.diospringsecurityjwt.model.User;
+
+public interface UserRepository extends JpaRepository<User, Integer> {
+	
+	@Query("SELECT e FROM User e JOIN FETCH e.roles WHERE e.username= (:username)")
+	public User findByUsername(@Param("username") String username);
+
+	boolean existsByUsername(String username);
+}
