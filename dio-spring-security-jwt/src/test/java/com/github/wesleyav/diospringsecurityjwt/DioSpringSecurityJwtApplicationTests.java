63cbb430cb7e8ba077e307dc3c91c37522a7089e
Wesley AV <13851280+wesleyav@users.noreply.github.com>
2022-08-03 17:30:28 -0300

    JWT - JSON Web Token - Parte 1

---
+++ dio-spring-security-jwt/src/test/java/com/github/wesleyav/diospringsecurityjwt/DioSpringSecurityJwtApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.wesleyav.diospringsecurityjwt;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DioSpringSecurityJwtApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
