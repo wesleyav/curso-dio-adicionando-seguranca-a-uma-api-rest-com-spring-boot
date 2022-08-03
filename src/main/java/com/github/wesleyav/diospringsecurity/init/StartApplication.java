9c1e7ad80d47da77e02c5e441e4ec4e58290cae9
Wesley Araújo Vieira <wesley.contato@hotmail.com>
2022-08-03 17:06:00 -0300

    Autenticação com banco de dados

---
+++ src/main/java/com/github/wesleyav/diospringsecurity/init/StartApplication.java
@@ -0,0 +1,39 @@
+package com.github.wesleyav.diospringsecurity.init;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.stereotype.Component;
+import org.springframework.transaction.annotation.Transactional;
+
+import com.github.wesleyav.diospringsecurity.model.User;
+import com.github.wesleyav.diospringsecurity.repository.UserRepository;
+
+@Component
+public class StartApplication implements CommandLineRunner {
+
+	@Autowired
+	private UserRepository repository;
+
+	@Transactional
+	@Override
+	public void run(String... args) throws Exception {
+		User user = repository.findByUsername("admin");
+		if (user == null) {
+			user = new User();
+			user.setName("ADMIN");
+			user.setUsername("admin");
+			user.setPassword("master123");
+			user.getRoles().add("MANAGERS");
+			repository.save(user);
+		}
+		user = repository.findByUsername("user");
+		if (user == null) {
+			user = new User();
+			user.setName("USER");
+			user.setUsername("user");
+			user.setPassword("user123");
+			user.getRoles().add("USERS");
+			repository.save(user);
+		}
+	}
+}
